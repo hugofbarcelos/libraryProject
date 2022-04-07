2f75aea1cb8df750296448ea8f0c83f35e4ad478
Hugo Barcelos <hugo.filipesilvabarcelos@gmail.com>
2022-04-07 09:41:32 +0000

    added contacts, homepage and product components to app folder

---
+++ library/src/app/products/products.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  template: `
+  
+  `,
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
