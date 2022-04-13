c5b913d7fc375cc0a7c5a19ba7308da73fa05834
Hugo Barcelos <hugo.filipesilvabarcelos@gmail.com>
2022-04-13 20:28:33 +0000

    a little refactor

--- library/src/app/products/products.component.ts
+++ library/src/app/products/products.component.ts
@@ -1,94 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { interval, Observable } from 'rxjs';
 import { Book } from '../book.model';
 import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-products',
-  template: `
-  
-  <section>
-    <button (click)="filtered()">Read</button>
-    <input type="text" id="search-text" placeholder="Search book by name"/>
-    <button (click)="unfiltered()">!Read</button>
-  </section>
-  <section>
-
-    <article ngClass="boo" *ngFor="let b of book">
-      <h1>{{b.title}}</h1>
-      <p>{{b.author}}</p>
-      <img src="assets/livros/{{b.imageUrl}}">
-      <h1> Already read:</h1>
-      <button ngClass="alreadyRead">{{b.alreadyRead ? 'Yes' : 'No'}}</button>
-      <button (click)="deleteItem(b.id)">Delete</button>
-    </article>
-
-  </section>
-
-  `,
-  styles: [`
-
-
-    section {
-        width: 100%;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-    }
-    .alreadyRead {
-      display: flex;     
-      padding: 3px 7px;
-      border-radius: 5px;
-      color: white;
-      background-color: gray;
-      text-decoration: none;
-      font-size: 0.5em;
-      border: 0;
-      cursor: pointer;
-      margin: 1em;
-      text-align: center;
-    }
-    button {
-      display: flex;
-      justify-content: center;
-      padding: 3px 7px;
-      border-radius: 5px;
-      color: white;
-      background-color: gray;
-      text-decoration: none;
-      font-size: 0.5em;
-      border: 0;
-      cursor: pointer;
-      margin: 1em;
-      text-align: center;
-
-    }
-  `],
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
   providers: [{provide: BookService, useClass: BookService}],
 })
 export class ProductsComponent implements OnInit {
 
   book!: Book[];
-  book$!:Observable<Book[]>;
+  book$!: Observable<Book[]>;
+  bookServices!: BookService;
   
   constructor(private bookService : BookService) {
+      this.bookServices = bookService;
   }
 
   ngOnInit(): void {
-    this.book = this.bookService.getBooks();
+    this.book = this.bookServices.getBooks();
   }
 
   deleteItem(id: number){
-    this.book = this.bookService.getBooks().filter(b => b.id !== id);
-    return this.book;
+    this.book = this.bookServices.getBooks().filter(b => b.id !== id);
   }
 
   filtered(){
-    this.book = this.bookService.getBooks().filter(b => b.alreadyRead == true);
+    this.book = this.bookServices.getBooks().filter(({ alreadyRead }) => alreadyRead == true);
   }
 
   unfiltered(){
     this.book = this.bookService.getBooks().filter(b => b.alreadyRead !== true);
   }
-
 }
